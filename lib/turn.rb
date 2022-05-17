fbfb1ffc2a26140a5a2bf0feac73b219b8037f9e
Jared Shelby <jared.shelby@yale.edu>
2022-05-17 18:16:41 -0400

    commit tree a2c078daff03b1290af7d851a6f55ececbd68fbf

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,12 +1,4 @@
 ## TURN ##
-# - ask for input
-# - get input
-# - convert input to index
-# - if index is valid
-#   - make the move for index
-#   - show the board
-# - else
-#   - ask for input again until you get a valid input
 def turn(board)
     # ask for input
     puts "Please enter 1-9:"
